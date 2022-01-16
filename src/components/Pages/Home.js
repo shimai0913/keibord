9dacbd5e72cbb6294ed40a8e6aac72fc65de6d3a
shimai0913 <shimai0913@gmail.com>
2022-01-16 00:38:02 +0000

    firestore接続テスト

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,10 +1,15 @@
 import { useEffect } from 'react'
 import Box from '@mui/material/Box'
-import {} from 'common/Firebase'
+import { db } from 'common/Firebase'
+import { collection, getDocs } from 'firebase/firestore';
 
 export const Home = () => {
   useEffect(() => {
     console.log('useEffect発火')
+    const roomCollection = collection(db, 'room')
+    getDocs(roomCollection).then((querySnapshot) => {
+      querySnapshot.docs.forEach((doc) => console.log(doc.data()))
+    })
   }, [])
 
   return (
