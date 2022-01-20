f45d176225f9baa0eacb7d9b3068198fc947ee97
shimai0913 <shimai0913@gmail.com>
2022-01-20 07:25:59 +0000

    bordサイズをtheme管理

---
+++ src/common/theme/index.js
@@ -0,0 +1,8 @@
+const screenWidth = document.documentElement.clientWidth
+const screenHeight = document.documentElement.clientHeight
+// ボード横幅(px)
+export const bordWidth = screenWidth - 20
+// ボード縦幅(px)
+export const bordHeight = screenHeight - 20
+// バッジサイズ(px)
+export const badgeSize = 60
