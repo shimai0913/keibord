fccf4c62db874dfb906e21dfde0b7e8679f8ca01
shimai0913 <shimai0913@gmail.com>
2022-01-27 21:35:24 +0000

    theme追加

--- src/common/theme/index.js
+++ src/common/theme/index.js
@@ -1,8 +1,23 @@
-const screenWidth = document.documentElement.clientWidth
-const screenHeight = document.documentElement.clientHeight
+import { createTheme } from '@mui/material/styles'
+
+export const screenWidth = window.innerWidth
+export const screenHeight = window.innerHeight
 // ボード横幅(px)
-export const bordWidth = screenWidth - 20
+// export const bordWidth = screenWidth - 20
 // ボード縦幅(px)
-export const bordHeight = screenHeight - 20
+// export const bordHeight = screenHeight - 20
 // バッジサイズ(px)
 export const badgeSize = 60
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#ffffff',
+      contrast: '#000000'
+    },
+    secondary: {
+      main: '#000000',
+      contrast: '#ffffff'
+    }
+  }
+})
